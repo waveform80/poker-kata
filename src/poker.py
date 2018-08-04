39e72f2a6dd6f98e3b60a0bcbe120a0e20c10670
Dave Jones <dave@waveform.org.uk>
2018-08-04 21:19:54 +0100

    Dave's version

--- src/poker.py
+++ src/poker.py
@@ -1,68 +1,212 @@
-from itertools import product
+import re
+from itertools import product, groupby
 from random import shuffle
+from functools import total_ordering
+from operator import attrgetter
 
-VALUES = ('2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A')
-SUITS = ('H', 'D', 'S', 'C')
 
+@total_ordering
 class Card:
-    def __init__(self, value, suit):
-        value = str(value)
-        suit = str(suit)
-        if value not in VALUES:
-            raise ValueError
-        self.value = value
-        if suit not in SUITS:
-            raise ValueError
-        self.suit = suit
+    values = ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A')
+    suits = {
+        'S': '\N{PLAYING CARD ACE OF SPADES}',
+        'H': '\N{PLAYING CARD ACE OF HEARTS}',
+        'D': '\N{PLAYING CARD ACE OF DIAMONDS}',
+        'C': '\N{PLAYING CARD ACE OF CLUBS}',
+    }
+    _regex = re.compile(r'^(?P<value>[AJQK2-9]|10)\s*(?P<suit>[HDSC])$')
+
+    def __init__(self, s):
+        match = Card._regex.match(s)
+        if not match:
+            raise ValueError("%s is not a valid card spec" % s)
+        self._value = Card.values.index(match.group('value'))
+        self._suit = match.group('suit')
+
+    def __hash__(self):
+        return hash(str(self))
 
     def __repr__(self):
-        return '<Card object {}{}>'.format(self.value, self.suit)
+        return 'Card(%r)' % (self.value + self.suit)
+
+    def __str__(self):
+        return chr(ord(Card.suits[self.suit]) + {
+            # NOTE: Offset by 2 because of the "knight" card (C?!)
+            10: 12, # Q
+            11: 13, # K
+            12: 0,  # A
+        }.get(self.score, self.score + 1))
 
     def __eq__(self, other):
-        return self.value == other.value
+        return self.score == other.score
 
     def __lt__(self, other):
-        return self.value_index < other.value_index
-
-    def __gt__(self, other):
-        return self.value_index > other.value_index
+        return self.score < other.score
 
     @property
-    def value_index(self):
-        return VALUES.index(self.value)
+    def suit(self):
+        return self._suit
+
+    @property
+    def value(self):
+        return Card.values[self.score]
+
+    @property
+    def score(self):
+        return self._value
 
 
-class PokerHand:
+@total_ordering
+class Hand:
+    values = (
+        'high card',
+        'pair',
+        'two pair',
+        'three of a kind',
+        'straight',
+        'flush',
+        'full house',
+        'four of a kind',
+        'straight flush',
+        'royal flush',
+    )
+
     def __init__(self, cards):
-        if any(not isinstance(card, Card) for card in cards):
-            raise ValueError
+        if isinstance(cards, Deck):
+            cards = cards.deal(5)
+        elif isinstance(cards, str):
+            cards = [Card(s) for s in cards.split()]
+        else:
+            cards = [card if isinstance(card, Card) else Card(card)
+                     for card in cards]
         if len(cards) != 5:
-            raise ValueError
-        card_set = {repr(card) for card in cards}
-        duplicate_cards = len(card_set) < len(cards)
-        if duplicate_cards:
-            raise ValueError
-        self.cards = sorted(cards)
+            raise ValueError('hand must consist of 5 cards')
+        self._cards = sorted(cards)
 
     def __repr__(self):
-        return '<PokerHand object {}>'.format(', '.join(str(card) for card in self))
+        return 'Hand([%s])' % ', '.join(repr(card) for card in self)
+
+    def __str__(self):
+        return '%s  \N{RIGHTWARDS DOUBLE ARROW} %s' % (
+            ' '.join(str(card) for card in self),
+            self.hand
+        )
 
     def __iter__(self):
-        return iter(self.cards)
+        return iter(self._cards)
 
-    def __gt__(self, other):
-        return self.cards[-1] > other.cards[-1]
+    def __getitem__(self, index):
+        return self._cards[index]
 
-    def __lt__(self, other):
-        return self.cards[-1] < other.cards[-1]
+    def __len__(self):
+        return 5
+
+    def __contains__(self, card):
+        # NOTE: this checks *identity* rather than equality
+        return any(c is card for c in self)
 
     def __eq__(self, other):
-        return self.cards[-1] == other.cards[-1]
+        return self.score == other.score and all(
+            sc.score == oc.score for sc, oc in zip(self, other)
+        )
 
-def create_deck():
-    cards = [Card(value, suit) for value, suit in product(VALUES, SUITS)]
-    shuffle(cards)
-    return cards
+    def __lt__(self, other):
+        if self.score < other.score:
+            return True
+        elif self.score == other.score:
+            for sc, oc in reversed(list(zip(self, other))):
+                if sc.score < oc.score:
+                    return True
+                elif sc.score > oc.score:
+                    break
+        return False
 
-def deal_hand(n, deck):
-    return [deck.pop() for i in range(n)]
+    @property
+    def hand(self):
+        scores = [card.score for card in self]
+        is_straight = (
+            # special case: 2 3 4 5 A
+            scores == [0, 1, 2, 3, 12] or
+            # general case
+            scores == list(range(self[0].score, self[0].score + 5))
+        )
+        if all(c.suit == self[0].suit for c in self[1:]):
+            if scores == [8, 9, 10, 11, 12]:
+                return 'royal flush'
+            elif is_straight:
+                return 'straight flush'
+            else:
+                return 'flush'
+        else:
+            value_runs = tuple(sorted(
+                len(list(group))
+                for key, group in groupby(self, attrgetter('value'))
+            ))
+            return {
+                (1, 4): 'four of a kind',
+                (2, 3): 'full house',
+                (1, 1, 3): 'three of a kind',
+                (1, 2, 2): 'two pair',
+                (1, 1, 1, 2): 'pair',
+                (1, 1, 1, 1, 1): 'straight' if is_straight else 'high card',
+            }[value_runs]
+
+    @property
+    def score(self):
+        return Hand.values.index(self.hand)
+
+    def exchange(self, deck, *cards):
+        # XXX Should check all cards are in self before redrawing any
+        for card in cards:
+            self._cards.remove(card)
+            deck.discard(card)
+            self._cards.append(deck.draw())
+        self._cards.sort()
+
+
+class Deck:
+    def __init__(self):
+        self._cards = [
+            Card(value + suit)
+            for value, suit in product(Card.values, Card.suits)
+        ]
+        self._discard = []
+        shuffle(self._cards)
+
+    def __repr__(self):
+        return '<Deck cards=%d discard=%d>' % (
+            len(self._cards), len(self._discard)
+        )
+
+    def __iter__(self):
+        return iter(self._cards)
+
+    def __getitem__(self, index):
+        return self._cards[index]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __contains__(self, card):
+        # NOTE: this checks *identity* rather than equality (see discard())
+        return any(c is card for c in self)
+
+    def draw(self):
+        try:
+            return self._cards.pop()
+        except IndexError:
+            raise ValueError('cannot draw from empty deck')
+
+    def deal(self, n=5):
+        if len(self) < n:
+            raise ValueError('insufficient cards left to deal')
+        return [self.draw() for i in range(n)]
+
+    def discard(self, card):
+        assert card not in self
+        self._discard.append(card)
+
+    def reshuffle(self):
+        self._cards.extend(self._discard)
+        self._discard = []
+        shuffle(self._cards)
--- src/tests.py
+++ src/tests.py
@@ -1,48 +1,78 @@
-from poker import Card, PokerHand, create_deck, deal_hand
-
-# some useful card suit/value constants
-T = 'T'
-J = 'J'
-Q = 'Q'
-K = 'K'
-A = 'A'
-H = 'H'
-D = 'D'
-S = 'S'
-C = 'C'
-# you can use integers for the others (but not 10 - use T)
+from poker import Card, Hand, Deck
 
 # test single card
-card_1 = Card(A, S)
-card_2 = Card(A, D)
+card_1 = Card('AS')
+card_2 = Card('AD')
+card_3 = Card('KS')
 assert card_1 == card_2
-card_3 = Card(K, S)
 assert card_1 != card_3
 assert card_1 > card_3
 assert not card_1 < card_3
 
 # test comparing two poker hands
-cards = [Card(v, s) for v, s in [(A, S), (2, D), (4, C), (7,  H), (K, S)]]
-hand_1 = PokerHand(cards)
-cards = [Card(v, s) for v, s in [(A, C), (3, D), (5, C), (8,  H), (T, S)]]
-hand_2 = PokerHand(cards)
+hand_1 = Hand('AS 2D 4C 7H KS')
+hand_2 = Hand('AC 2C 4D 7S KH')
 assert hand_1 == hand_2
 assert hand_2 == hand_1
 
-cards = [Card(v, s) for v, s in [(A, S), (2, D), (4, C), (7,  H), (K, S)]]
-hand_1 = PokerHand(cards)
-cards = [Card(v, s) for v, s in [(Q, S), (3, D), (5, C), (8,  H), (T, S)]]
-hand_2 = PokerHand(cards)
+hand_1 = Hand('AS 2D 4C 7H KS')
+hand_2 = Hand('QS 3D 5C 8H 10S')
 assert hand_1 > hand_2
 assert hand_2 < hand_1
 
+hand_1 = Hand('2H 3D 4S 9C KD')
+hand_2 = Hand('2H 3D 6S 8C KD')
+assert hand_1 > hand_2
+assert hand_2 < hand_1
+
+# test comparing all valid hand types
+royal_flush      = Hand('AS 10S JS QS KS')
+straight_flush_1 = Hand('AS 2S 3S 4S 5S')
+straight_flush_2 = Hand('6S 2S 3S 4S 5S')
+flush            = Hand('AS 2S 3S 4S 6S')
+four_kind        = Hand('AS AC AD AH 5S')
+full_house       = Hand('AS AC AD 5H 5S')
+straight_1       = Hand('AH 2S 3S 4S 5S')
+straight_2       = Hand('6S 2D 3S 4S 5S')
+three_kind       = Hand('AS AC AD 4H 5S')
+two_pair         = Hand('AS AC 4D 4H 5S')
+pair             = Hand('AS 2C 4D 4H 5S')
+high_card_1      = Hand('AS 2C 4D 7H 5S')
+high_card_2      = Hand('9S 2C 4D 7H 5S')
+assert royal_flush.hand == 'royal flush'
+assert straight_flush_1.hand == straight_flush_2.hand == 'straight flush'
+assert flush.hand == 'flush'
+assert straight_1.hand == straight_2.hand == 'straight'
+assert four_kind.hand == 'four of a kind'
+assert full_house.hand == 'full house'
+assert three_kind.hand == 'three of a kind'
+assert two_pair.hand == 'two pair'
+assert pair.hand == 'pair'
+assert high_card_1.hand == high_card_2.hand == 'high card'
+
+assert (
+    royal_flush
+    > straight_flush_1
+    > straight_flush_2
+    > four_kind
+    > full_house
+    > flush
+    > straight_1
+    > straight_2
+    > three_kind
+    > two_pair
+    > pair
+    > high_card_1
+    > high_card_2
+)
+
 # test creating a deck
-deck = create_deck()
+deck = Deck()
 assert len(deck) == 52
 assert all(isinstance(card, Card) for card in deck)
 
 # test creating a hand from the deck
-cards = deal_hand(5, deck)
-hand_1 = PokerHand(cards)
-cards = deal_hand(5, deck)
-hand_2 = PokerHand(cards)
+hand_1 = Hand(deck)
+hand_2 = Hand(deck)
+assert len(hand_1) == 5
+assert len(hand_2) == 5
